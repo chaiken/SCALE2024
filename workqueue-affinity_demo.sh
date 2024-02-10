4393f22ef77a5b294ff825d3984b7f57ee33872e
Alison Chaiken <alison@she-devel.com>
2024-02-10 15:41:26 -0800

    workqueue-affinity-demo: change tunable to nice
    Change the tunable workqueue parameter that is varied during the demo to
    niceness since setting cpumask does not actually trigger formation of a new pool
    for unbound workqueues.   Change the delays from "sleep" to "Press any key to
    continue" in order to allow viewers to differentiate between the execution time
    of drgn/BPF and intentional waiting.   Change the victim workqueue to
    nvme-delete-wq since both laptop and PC run it.

--- workqueue-affinity_demo.sh
+++ workqueue-affinity_demo.sh
@@ -4,83 +4,114 @@ set -e
 set -u
 
 readonly KERNEL_PATH=/home/alison/gitsrc/linux-trees/linux/tools/workqueue
-readonly WORKQUEUE=raid5wq
+readonly WORKQUEUE=nvme-delete-wq
 readonly SYSFS_PATH=/sys/devices/virtual/workqueue/${WORKQUEUE}
 readonly DEFAULT_AFFINITY_SCOPE=$(cat /sys/devices/virtual/workqueue/${WORKQUEUE}/affinity_scope | cut -f 1 -d " ")
-readonly DEFAULT_CPUMASK=$(cat /sys/devices/virtual/workqueue/${WORKQUEUE}/cpumask)
+readonly DEFAULT_NICE=$(cat /sys/devices/virtual/workqueue/${WORKQUEUE}/nice)
 
 restore() {
     echo "Restoring default settings"
     echo "${DEFAULT_AFFINITY_SCOPE}" > /sys/devices/virtual/workqueue/${WORKQUEUE}/affinity_scope
-    echo "${DEFAULT_CPUMASK}" > /sys/devices/virtual/workqueue/${WORKQUEUE}/cpumask
+    echo "${DEFAULT_NICE}" > /sys/devices/virtual/workqueue/${WORKQUEUE}/nice
+    echo ""
+    echo ""
 }
 
 main() {
 #0
     echo ""
+    echo "0. Workqueues which are configurable from sysfs:"
+    echo "$ ls /sys/devices/virtual/workqueue"
+    ls /sys/devices/virtual/workqueue
     echo ""
+
+    read -sn1 -p "Press any key to continue"
+
+
+#1
+    echo ""
+    echo ""
+    echo "1. Consider tunable parameterss for ${WORKQUEUE}:"
     echo "$ ls /sys/devices/virtual/${WORKQUEUE}"
     ls "$SYSFS_PATH"
     echo ""
 
-    sleep 5
+    read -sn1 -p "Press any key to continue"
 
-#1
-    echo "Default affinity scope of unbound ${WORKQUEUE} workqueue:"
+#2
+    echo ""
+    echo ""
+    echo "2. Default affinity scope of unbound ${WORKQUEUE} workqueue:"
     echo "$ cat /sys/devices/virtual/workqueue/${WORKQUEUE}/affinity_scope"
     cat "$SYSFS_PATH"/affinity_scope
     echo ""
 
-    sleep 5
-
-#2
-    echo "Default cpumask of unbound ${WORKQUEUE} workqueue:"
-    echo "$ cat /sys/devices/virtual/workqueue/${WORKQUEUE}/cpumask"
-    cat "$SYSFS_PATH"/cpumask
-    echo ""
-
-    sleep 5
+    read -sn1 -p "Press any key to continue"
 
 #3
-    echo "Determine in which workqueue pools ${WORKQUEUE} runs by default"
-    echo "Workqueue CPU -> pool"
-    echo "====================="
-    echo "[    workqueue     \     type   CPU  0  1  2  3  4  5  6  7 dfl]"
-    echo "$ drgn tools/workqueue/wq_dump.py | grep ${WORKQUEUE}"
-     drgn "$KERNEL_PATH"/wq_dump.py | grep ${WORKQUEUE}
+    echo ""
+    echo ""
+    echo "3. Default nice value of unbound ${WORKQUEUE} workqueue:"
+    echo "$ cat /sys/devices/virtual/workqueue/${WORKQUEUE}/nice"
+    cat "$SYSFS_PATH"/nice
     echo ""
 
-    sleep 5
+    read -sn1 -p "Press any key to continue"
 
 #4
-    echo "cpumask 15 = 1 + 2 + 4 + 8 --> cores 0-3"
-    echo "$  bash -c 'echo 15 > /sys/devices/virtual/workqueue/${WORKQUEUE}/cpumask'"
-     bash -c 'echo 15 > /sys/devices/virtual/workqueue/${WORKQUEUE}/cpumask'
+    echo ""
+    echo ""
+    echo "4. Determine in which workqueue pools ${WORKQUEUE} runs by default"
+    echo "Workqueue CPU -> pool"
+    echo "====================="
+    echo "[    workqueue     \     type   CPU  0  1  2  3  4  5  6  7 dfl]"
+    echo "$ drgn tools/workqueue/wq_dump.py | grep ${WORKQUEUE}"
+     drgn "$KERNEL_PATH"/wq_dump.py | grep ${WORKQUEUE}
     echo ""
 
-   sleep 5
+    read -sn1 -p "Press any key to continue"
 
 #5
-    echo "In which workqueue pools does ${WORKQUEUE} run now?"
+    echo ""
+    echo ""
+    echo "5. Set nice to 9"
+    echo "$  echo 9 > /sys/devices/virtual/workqueue/${WORKQUEUE}/nice"
+    echo 9 > /sys/devices/virtual/workqueue/${WORKQUEUE}/nice
+    echo ""
+
+   read -sn1 -p "Press any key to continue"
+
+#6
+    echo ""
+    echo ""
+    echo "6. In which workqueue pools does ${WORKQUEUE} run NOW?"
     echo "$ drgn tools/workqueue/wq_dump.py | grep ${WORKQUEUE}"
+    echo ""
+    echo ""
     echo "Workqueue CPU -> pool"
     echo "====================="
     echo "[    workqueue     \     type   CPU  0  1  2  3  4  5  6  7 dfl]"
      drgn "$KERNEL_PATH"/wq_dump.py | grep ${WORKQUEUE}
+    echo ""
     echo ""
 
     readonly POOL=$(drgn "$KERNEL_PATH"/wq_dump.py |  grep ${WORKQUEUE}  |  awk {'print $3;'})
 
-    sleep 5
-
-#6
-    echo "What else runs in workqueue pool ${POOL}?"
-    echo "$ drgn tools/workqueue/wq_dump.py | grep ${POOL}"
-    echo ""
-
-    sleep 5
+    read -sn1 -p "Press any key to continue"
 
 #7
+    echo ""
+    echo ""
+    echo "What else runs in workqueue pool ${POOL}?"
+    echo "$ drgn tools/workqueue/wq_dump.py | grep ${POOL}"
+    drgn "$KERNEL_PATH/"/wq_dump.py | grep ${POOL}
+    echo ""
+
+    read -sn1 -p "Press any key to continue"
+
+#8
+    echo ""
+    echo ""
     restore
 }
 
