42b2aa137d5b1f36094c0e704a3eb0e0d019678f
Alison Chaiken <alison@she-devel.com>
2024-02-19 14:52:59 -0800

    run-wq_monitor: fix printout

--- run-wq_monitor.sh
+++ run-wq_monitor.sh
@@ -7,13 +7,13 @@ readonly HOSTNAME="$(hostname)"
 readonly BOARDNAME=bullseye-dev64mq
 
 echo ""
-echo "The kernel is $(uname -r) from https://github.com/chaiken/linux/tree/wq-dump"
 echo ""
 
 if [[ "$BOARDNAME" == "$HOSTNAME" ]]
 then
     DRGN=/usr/local/bin/drgn
     BIN=/home/debian/wq_monitor.py
+    echo "The kernel is $(uname -r) from https://github.com/chaiken/linux/tree/wq-dump"
 else
     DRGN=/usr/bin/drgn
     export BIN=/home/alison/gitsrc/linux-trees/linux/tools/workqueue/wq_monitor.py
