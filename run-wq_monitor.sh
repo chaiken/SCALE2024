4cf487a72f711510716991822fe28105c9666197
Alison Chaiken <alison@she-devel.com>
2024-02-19 14:18:22 -0800

    run-wq_monitor: fixup path

--- run-wq_monitor.sh
+++ run-wq_monitor.sh
@@ -6,16 +6,19 @@ set -u
 readonly HOSTNAME="$(hostname)"
 readonly BOARDNAME=bullseye-dev64mq
 
-if [[ "$BOARDNAME" == "$HOSTNAME" ]]
-then
-    export BIN=/home/debian/gitsrc/linux-trees/linux/tools/workqueue/wq_monitor.py
-else
-    export BIN=/home/alison/gitsrc/linux-trees/linux/tools/workqueue/wq_monitor.py
-fi
-
 echo ""
 echo "The kernel is $(uname -r) from https://github.com/chaiken/linux/tree/wq-dump"
 echo ""
-echo "$ /usr/bin/drgn ${BIN}"
 
-/usr/bin/drgn "$BIN"
+if [[ "$BOARDNAME" == "$HOSTNAME" ]]
+then
+    DRGN=/usr/local/bin/drgn
+    BIN=/home/debian/wq_monitor.py
+else
+    DRGN=/usr/bin/drgn
+    export BIN=/home/alison/gitsrc/linux-trees/linux/tools/workqueue/wq_monitor.py
+fi
+
+echo "$ ${DRGN} ${BIN}"
+
+"$DRGN" "$BIN"
